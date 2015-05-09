5db15616136882d719fbce74ba45ddad7ce5fc05
Michele Mattioni <mattions@gmail.com>
2015-05-09 18:55:36 +0100

    We need to pass the port as well

--- wp-config.php
+++ wp-config.php
@@ -16,7 +16,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', getenv('DB_NAME'));
+define('DB_NAME', getenv('DB_NAME') . ":" . getenv('DB_PORT'));
 
 /** MySQL database username */
 define('DB_USER', getenv('DB_USER'));
